879058a6adc94648afb02b9de8b58173e4daf058
Kaushigithub <105336807+Kaushigithub@users.noreply.github.com>
2023-09-09 07:20:38 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,21 @@
+/*
+============================================================================
+Name : 2.c
+Author : Kauhal Yogeshbhai Pancholi
+Description : Write a simple program to execute in an infinite loop at the background. Go to /proc directory 
+	      and identify all the process related information in the corresponding proc directory.
+Date: 25th Aug, 2023.
+============================================================================
+*/
+
+
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+        while(1){
+        }
+        return 0;
+}
+
+/* Command to get information about process - cat /proc/pid/status     */  
